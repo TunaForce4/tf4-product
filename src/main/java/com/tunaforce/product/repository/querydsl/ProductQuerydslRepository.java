437b38626653faeafab49945b78568e0e1b84d01
legoChoi <chlx159@gmail.com>
2025-08-20 13:06:39 +0900

    :art: refactor: modify codes

--- src/main/java/com/tunaforce/product/repository/querydsl/ProductQuerydslRepository.java
+++ src/main/java/com/tunaforce/product/repository/querydsl/ProductQuerydslRepository.java
@@ -8,11 +8,23 @@ import java.util.UUID;
 
 public interface ProductQuerydslRepository {
 
+    /**
+     * 상품 단건 조회 쿼리
+     */
     ProductDetailsQuerydslResponseDto getProductDetails(UUID productId);
 
+    /**
+     * 주문용 상품 전체 조회 쿼리
+     */
     Page<ProductDetailsQuerydslResponseDto> findPage(Pageable pageable, String productName);
 
+    /**
+     * 허브 담당자용 허브 내 업체 등록 상품 조회 쿼리
+     */
     Page<ProductDetailsQuerydslResponseDto> findPageForHub(Pageable pageable, UUID hubId, String productName);
 
+    /**
+     * 업체 담당자용 업체 등록 상품 조회 쿼리
+     */
     Page<ProductDetailsQuerydslResponseDto> findPageForCompany(Pageable pageable, UUID companyId, String productName);
 }
