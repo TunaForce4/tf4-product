fc4cff23059252f437a4b8d6ccca1aee14e34222
legoChoi <chlx159@gmail.com>
2025-08-21 09:20:57 +0900

    :sparkles: feat: add internal APIs for order-service

---
+++ src/main/java/com/tunaforce/product/controller/ProductInternalController.java
@@ -0,0 +1,65 @@
+package com.tunaforce.product.controller;
+
+import com.tunaforce.product.dto.request.ProductUpdateOrderQuantityRequestDto;
+import com.tunaforce.product.dto.request.ProductSimpleListRequestDto;
+import com.tunaforce.product.dto.request.ProductSimpleReduceStockRequestDto;
+import com.tunaforce.product.dto.response.ProductUpdateOrderQuantityResponseDto;
+import com.tunaforce.product.dto.response.ProductSimpleListResponseDto;
+import com.tunaforce.product.dto.response.ProductSimpleResponseDto;
+import com.tunaforce.product.dto.response.ProductSimpleReduceStockResponseDto;
+import com.tunaforce.product.service.ProductInternalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/internal/products")
+@RequiredArgsConstructor
+public class ProductInternalController {
+
+    private final ProductInternalService productInternalService;
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductSimpleResponseDto> findProduct(
+            @PathVariable UUID productId
+    ) {
+        ProductSimpleResponseDto data = productInternalService.findProduct(productId);
+
+        return ResponseEntity.ok()
+                .body(data);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductSimpleListResponseDto> findProducts(
+            @RequestBody ProductSimpleListRequestDto productSimpleListRequestDto
+    ) {
+        ProductSimpleListResponseDto data = productInternalService.findProducts(productSimpleListRequestDto);
+
+        return ResponseEntity.ok()
+                .body(data);
+    }
+
+    @PatchMapping("/{productId}/reduce-stock")
+    public ResponseEntity<ProductSimpleReduceStockResponseDto> reduceStock(
+            @PathVariable UUID productId,
+            @RequestBody ProductSimpleReduceStockRequestDto productSimpleReduceStockRequestDto
+    ) {
+        ProductSimpleReduceStockResponseDto data = productInternalService.reduceStock(productId, productSimpleReduceStockRequestDto);
+
+        return ResponseEntity.ok()
+                .body(data);
+    }
+
+    @PatchMapping("/{productId}/update-order-quantity")
+    public ResponseEntity<ProductUpdateOrderQuantityResponseDto> updateOrderQuantity(
+            @PathVariable UUID productId,
+            @RequestBody ProductUpdateOrderQuantityRequestDto productUpdateOrderQuantityRequestDto
+    ) {
+        ProductUpdateOrderQuantityResponseDto data = productInternalService.updateOrderQuantity(productId, productUpdateOrderQuantityRequestDto);
+
+        return ResponseEntity.ok()
+                .body(data);
+    }
+}
